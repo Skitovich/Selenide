fbf31205b9099734152dbf77bde29b5ce5156d91
Skitovich Ruslan <zloycvetok@gmail.com>
2020-11-24 12:31:02 +0300

    edit test shouldShowErrorCheckBoxTest

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,9 +1,11 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.conditions.CssClass;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,13 +21,12 @@ class CallbackTest {
 
     }
 
-
     @Test
     public void shouldShowErrorTest() {
         open("http://localhost:9999");
         $(By.name("phone")).setValue("+79150000000");
         $(By.className("button__text")).click();
-        $("[class=input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
 
@@ -33,12 +34,11 @@ class CallbackTest {
     public void shouldShowErrorCheckBoxTest() {
         open("http://localhost:9999");
         $(By.name("phone")).setValue("+79150000000");
+//        $(By.className("checkbox__box")).click();
         $(By.name("name")).setValue("Руслан Скитович");
         $(By.className("button__text")).click();
-        $("[class=input_invalid").exists();
+        $("[input_invalid]").exists();
 
     }
-
-
 }
 
