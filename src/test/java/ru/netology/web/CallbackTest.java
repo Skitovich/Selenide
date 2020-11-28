d5c05c94a833ee1535a9fbe16ff7853ba8684438
Skitovich Ruslan <zloycvetok@gmail.com>
2020-11-28 21:41:10 +0300

    edit test after check

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,44 +1,61 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.conditions.CssClass;
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byAttribute;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 class CallbackTest {
-    @Test
-    public void shouldRegisteredTest() {
-        open("http://localhost:9999");
-        $(By.name("name")).setValue("Руслан Скитович");
-        $(By.name("phone")).setValue("+79150000000");
-        $(By.className("checkbox__box")).click();
-        $(By.className("button__text")).click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
 
     }
 
     @Test
-    public void shouldShowErrorTest() {
+    public void shouldRegisteredTest() {
         open("http://localhost:9999");
-        $(By.name("phone")).setValue("+79150000000");
-        $(By.className("button__text")).click();
-        $("[class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
+        element(By.name("name")).setValue("Руслан Скитович");
+        element(By.name("phone")).setValue("+79150000000");
+        element(By.className("checkbox__box")).click();
+        element(By.className("button__text")).click();
+        element("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+
+    @Test
+    public void shouldShowErrorTestWithoutName() {
+        open("http://localhost:9999");
+        element(By.name("phone")).setValue("+79150000000");
+        element(By.className("button__text")).click();
+        element("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void shouldShowErrorTestWithoutTelephone() {
+        open("http://localhost:9999");
+        element(By.name("name")).setValue("Руслан Скитович");
+        element(By.className("button__text")).click();
+        element("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
 
     @Test
     public void shouldShowErrorCheckBoxTest() {
         open("http://localhost:9999");
-        $(By.name("phone")).setValue("+79150000000");
-        $(By.name("name")).setValue("Руслан Скитович");
-        $(By.className("button__text")).click();
-        $(".input_invalid").should(exist);
+        element(By.name("phone")).setValue("+79150000000");
+        element(By.name("name")).setValue("Руслан Скитович");
+        element(By.className("button__text")).click();
+        element(".form-field .input_invalid").should(exist);
+        element(".form-field .checkbox__text").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+
 
     }
-}
 
+}
